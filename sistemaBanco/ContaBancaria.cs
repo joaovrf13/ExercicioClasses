04112df3af7236e42c46264228b0121f5f3dec9d
JoaoVrf13 <joaovictorrfcontato@gmail.com>
2024-09-05 17:26:02 -0300

    Criação do programa

---
+++ sistemaBanco/ContaBancaria.cs
@@ -0,0 +1,59 @@
+﻿using System.Globalization;
+
+
+namespace sistemaBanco
+{
+    internal class Cliente
+    {
+
+        private string Nome { get; set; }
+        private int NumeroConta { get; set; }
+        private double Saldo { get; set; }
+
+        public Cliente(string nome, int numeroConta, double saldo)
+        {
+            Nome = nome;
+            NumeroConta = numeroConta;
+            Saldo = saldo;
+        }
+
+        public Cliente(int numeroconta, string nome)
+        {
+
+            NumeroConta = numeroconta;
+            Nome = nome;
+        }
+
+        public Cliente(int numeroconta, string nome, double depositoInicial)
+        {
+            Depositar(depositoInicial);
+        }
+
+
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+
+        }
+
+        public void Retirar(double valor)
+        {
+            Saldo -= valor + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Número da conta:  "
+                   + NumeroConta.ToString()
+                   + ", "
+                   + " Nome do titular: "
+                   + Nome
+                   + ", "
+                   + "Saldo: "
+                   + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+    }
+}
---
+++ sistemaBanco/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace sistemaBanco {
+    class Program {
+        static void Main(string[] args) {
+
+            Cliente c;
+
+            Console.WriteLine("Entre com o número da conta: ");
+            int numeroconta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Haverá depósito inicial?");
+            Console.WriteLine("1 - Sim\n" +
+                              "2 - Não\n");
+
+            int opcao = int.Parse(Console.ReadLine());
+
+            double saldoinicial = 0.0;
+
+            if (opcao == 1) {
+
+                Console.WriteLine("Digite o valor para depósito: ");
+                saldoinicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c = new Cliente(numeroconta, nome, saldoinicial);
+                Console.WriteLine("Dados da conta: " + c);
+                Console.WriteLine("Digite um valor para sacar: ");
+                double sacar = double.Parse(Console.ReadLine());
+                c.Retirar(sacar);
+            }
+            else if (opcao == 2) {
+                c = new Cliente(numeroconta, nome);
+                Console.WriteLine($"Dados da conta: {c}");
+                Console.WriteLine();
+                Console.WriteLine("Entre com um valor para depósito: ");
+                double valordeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c.Depositar(valordeposito);
+                Console.WriteLine("Dados da conta atualizados: ");
+                Console.WriteLine(c);
+
+                Console.WriteLine("Digite um valor para sacar: ");
+                valordeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c.Retirar(valordeposito);
+                Console.WriteLine(c);
+
+            }
+            else {
+                Console.WriteLine("ERROR Opção Invalida! ");
+                Console.WriteLine("DIGITE UMA OPÇÂO DISPONIVEL");
+                return;
+            }
+
+
+
+
+        }
+    }
+}
